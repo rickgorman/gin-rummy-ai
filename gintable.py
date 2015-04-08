6d9ab97cecc9b48ef7cb1ec6a3eb7495e28fffc7
rickgorman <gormanite@gmail.com>
2015-04-08 00:36:50 -1000

    Added and passed tests for GinTable.organize_data()

--- gintable.py
+++ gintable.py
@@ -9,10 +9,12 @@
 
 from ginplayer import *
 from gindeck import *
+from observer import *
 
 
-class GinTable(object):
+class GinTable(Observable):
     def __init__(self):
+        super(GinTable, self).__init__()
         self.player1 = False
         self.player2 = False
 
@@ -40,6 +42,26 @@ class GinTable(object):
             return True
         else:
             raise TableSeatingError("gintable is full")
+
+    def organize_data(self):
+        data = {}
+
+        # we start with the deck cards. data[0] holds the bottom card of the deck. we pad data[] with 0's once we
+        #   run out of cards to ensure we return 52 items for the deck
+        deck_size = len(self.deck.cards)
+        for i in range(deck_size):
+            data[i] = self.deck.cards[i].ranking()
+        for i in range(deck_size, 52):
+            data[i] = 0
+
+        # we do the same for the discard pile. the bottommost card is loaded first.
+        discard_size = len(self.discard_pile)
+        for i in range(deck_size, deck_size + discard_size):
+            data[i] = self.discard_pile[i-deck_size].ranking()
+        for i in range(deck_size + discard_size, 104):
+            data[i] = 0
+
+        return data
 
 
 class TableSeatingError(Exception):
--- test_gintable.py
+++ test_gintable.py
@@ -34,3 +34,41 @@ class TestGinTable(unittest.TestCase):
         self.assertEqual(self.t.player1, self.p1)
 
         self.assertRaises(TableSeatingError, self.t.seat_player, self.p1)
+
+    # ensure we return the current deck, as well as the discard pile
+    def test_organize_data(self):
+        # we move a few cards into the discard pile
+        self.t.discard_pile.append(self.t.deck.cards.pop())
+        self.t.discard_pile.append(self.t.deck.cards.pop())
+        self.t.discard_pile.append(self.t.deck.cards.pop())
+
+        data = self.t.organize_data()
+
+        # we expect 52 entries for deck and 52 for discard pile
+        self.assertEqual(104, len(data))
+
+        # we should find a total of 52 0's, where 0 means there is no card there.
+        zeroes = 0
+
+        # record uniqueness of each card
+        found = {}
+        for i in range(104):
+            if data[i] is 0:
+                zeroes += 1
+            else:
+                self.assertTrue(data[i] not in found.keys())
+
+            found[data[i]] = True
+
+        # ensure we found 53 unique items
+        self.assertEqual(53, len(found))
+        # in all, 52 zeroes should be found between the deck and the discard pile.
+        self.assertEqual(52, zeroes)
+
+        ordered = sorted(found.keys())
+        for i in range(0, len(found.keys())):
+            self.assertEqual(i, ordered[i])
+
+        # here we ensure the values are tightly contained within [0,52]
+        self.assertTrue(min(found.keys()) >= 0)
+        self.assertTrue(max(found.keys()) == 52)
